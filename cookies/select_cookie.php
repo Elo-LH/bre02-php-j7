c9915ec7e36420b2c4b2d39624ad18a3c9c2c810
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-05-27 10:21:19 +0200

    cookie

---
+++ cookies/select_cookie.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Select cookie</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label for="color-select">Choose a text color:</label>
+        <select name="color" id="color-select">
+  <option value="default">Black</option>
+  <option value="blue">Blues</option>
+
+  <option value="red">Red</option>
+  </select>
+<button type="sumbit">Apply</button>
+    </form>
+    
+</body>
+</html>

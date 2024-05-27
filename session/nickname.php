300b323770c2253cf6149b1840d4c2ad340eb1f7
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-05-27 09:59:07 +0200

    étape 1: session

---
+++ session/nickname.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nickname</title>
+</head>
+<body>
+    <form action="welcome.php" method="get">
+<label for="nickname">Choose a nickname:</label>
+<input type="text" name="nickname" id="nickname">
+<button>Enter site</button>
+    </form>
+    
+</body>
+</html>
---
+++ session/welcome.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!array_filter($_GET)){
+   $nickname = "guest";
+  }else{
+      if(isset($_GET['nickname'])){
+         
+           $nickname = $_GET['nickname'];
+         }
+      
+  }
+
+  ?>
+
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+  </head>
+  <body>
+    <h1>Welcome <?=$nickname?></h1>
+  </body>
+  </html>
